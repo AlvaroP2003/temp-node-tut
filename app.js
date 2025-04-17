3208968c963393dce97553c8e708d17d67a3d91b
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-04-17 10:04:16 +0200

    Added node files

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-// npm - global command, comes with node
-// npm --version
+// NB* Callback functions are run after immediate code
 
-// local dependancy - use it only in this particular project
-//  - npm i <packageName>
+const {readFile} = require('fs')
 
-// global dependancy - use it on any project
-//  - npm install -g <packageName>
-//  - sudo install -g <packageName> (mac)
+console.log('started the first task');
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-
-
-// lodash package example
-const _  = require('lodash');
-
-const items = [1,[2,[3,[4]]]]
-
-const newItems = _.flattenDeep(items)
-
-console.log(newItems);
+// CHECK FILE PATH
+readFile('./content/first.txt', 'utf-8', (err,result) => {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log(result);
+    console.log('completed first task');
+})
+console.log('starting next task');

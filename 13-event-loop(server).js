1175b9f2cfd0a639731661b839a3b04fcf89fe60
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-04-17 11:44:56 +0200

    Added new lessons

---
+++ 13-event-loop(server).js
@@ -0,0 +1,13 @@
+const http = require('server')
+
+const server = http.createServer((req,res) => {
+    console.log('request event');
+    res.end('Hello World!')
+})
+
+const port = 5000
+
+server.listen(port , () => {
+    console.log(`Server listening on port ${port}...`);
+    
+})

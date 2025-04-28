05ce6a6fe07fd3540a212bfd622a58898d60daf0
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-04-28 10:42:24 +0200

    Added files to pubcli folder

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const path = requrie('path')
+const path = require('path')
 const app = express()
 
+// setup static and middleware
+app.use(express.static('./public'))
 
-app.get('/',(res,res)=> {
-    res.sendFile(path.resolve(__dirname,'./navbar.app/index.html'))
-})
+// app.get('/',(res,res)=> {
+//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
+
+
 
 app.all('*',(req,res)=> {
     res.status(404).send('Page not found')

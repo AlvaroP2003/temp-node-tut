b447cf9928171f2bb961509abb08d8ecdc9a617b
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-04-28 10:34:52 +0200

    Added new express fucntionality

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const express = require('express')
+const path = requrie('path')
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.status(200).send('Home Page')
-})
 
-app.get('/about',(req,res)=> {
-    res.status(200).send('About Page')
+app.get('/',(res,res)=> {
+    res.sendFile(path.resolve(__dirname,'./navbar.app/index.html'))
 })
 
 app.all('*',(req,res)=> {
     res.status(404).send('Page not found')
 })
 
-app.listen(5000, ()=> {
-    console.log('Server is listening on port 5000...');
+app.listen(5000,()=> {
+    console.log('Server listening on port 5000');
 })
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
-
